5be5e00f6465d60e234ea8aceb0a6e85c84d1ee5
EnesUnlu <enes9103@gmail.com>
2022-04-11 21:45:14 +0300

    Added Approuter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
+import AppRouter from "./router/AppRouter";
 
 const App = () => {
   return (
     <div>
-      <h2>Movie App</h2>
+      <AppRouter />
     </div>
   );
 };
--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,1 +1,20 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Main from '../pages/Main'
+import Login from '../pages/Login'
+import Register from '../pages/Register'
+
+
+const AppRouter = () => {
+  return (
+    <Router>
+        <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+        </Routes>
+    </Router>
+  )
+}
+
+export default AppRouter
 
