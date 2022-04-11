551d2560ac60e9938347d7a0861be5d8797fdf61
EnesUnlu <enes9103@gmail.com>
2022-04-11 22:08:36 +0300

    Added Navbar

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -2,11 +2,13 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Main from '../pages/Main'
 import Login from '../pages/Login'
 import Register from '../pages/Register'
+import Navbar from '../components/Navbar'
 
 
 const AppRouter = () => {
   return (
     <Router>
+        <Navbar/>
         <Routes>
             <Route path="/" element={<Main />} />
             <Route path="/login" element={<Login />} />
