a88b1b5d9fcd2a9ed7a265ce75136e8c4a6f1438
EnesUnlu <enes9103@gmail.com>
2022-04-14 00:32:20 +0300

    private route

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,12 +1,27 @@
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Routes, Route, Navigate, Outlet, useLocation} from 'react-router-dom'
 import Main from '../pages/Main'
 import Login from '../pages/Login'
 import Register from '../pages/Register'
 import Navbar from '../components/Navbar'
 import MovieDetail from '../pages/MovieDetail'
+import { AuthContext } from '../context/AuthContext'
+import { useContext } from 'react'
 
 
 const AppRouter = () => {
+  const { currentUser } = useContext(AuthContext)
+  function PrivateRouter() {
+    let location = useLocation();
+    if (!currentUser) {
+      // Redirect them to the /login page, but save the current location they were
+      // trying to go to when they were redirected. This allows us to send them
+      // along to that page after they login, which is a nicer user experience
+      // than dropping them off on the home page.
+      return <Navigate to="/login" state={{ from: location }} replace />;
+    }
+
+    return <Outlet />;
+  }
   return (
     <Router>
         <Navbar/>
@@ -14,7 +29,11 @@ const AppRouter = () => {
             <Route path="/" element={<Main />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
-            <Route path="/details/:id" element={<MovieDetail/>} />
+            <Route element={<PrivateRouter/>} >
+                <Route path="/detail/:id" element={<MovieDetail />} />
+            </Route>
+            {/* <Route path="/details/:id" element={<MovieDetail/>} /> */}
+            {/* <Route path="/details/:id" element={currentUser ? <MovieDetail/> : <Navigate to ="/login" replace/>} /> */}
         </Routes>
     </Router>
   )
